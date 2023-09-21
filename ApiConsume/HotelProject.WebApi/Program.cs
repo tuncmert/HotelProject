4dae284ef8e8c05d546cbbdb0750bd65291c5da4
tuncmert <merttunc20@gmail.com>
2023-09-21 22:14:15 +0300

    create admin-guest

--- ApiConsume/HotelProject.WebApi/Program.cs
+++ ApiConsume/HotelProject.WebApi/Program.cs
@@ -33,6 +33,9 @@ builder.Services.AddScoped<IBookingService, BookingManager>();
 builder.Services.AddScoped<IContactDal, EfContactDal>();
 builder.Services.AddScoped<IContactService, ContactManager>();
 
+builder.Services.AddScoped<IGuestDal, EfGuestDal>();
+builder.Services.AddScoped<IGuestService, GuestManager>();
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 //builder.Services.AddS
