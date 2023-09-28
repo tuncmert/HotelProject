9698a96bd449b7379e00158b2eeb12d5efe12fb7
tuncmert <merttunc20@gmail.com>
2023-09-28 23:38:02 +0300

    send message

--- Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -37,13 +37,16 @@ namespace HotelProject.WebUI.Controllers
         [HttpPost]
         public async Task<IActionResult> AddSendMessage(CreateSendMessageDto model)
         {
+            model.SenderName = "Admin";
+            model.SenderMail = "admin@mail.com";
+            model.Date = DateTime.Parse(DateTime.Now.ToShortDateString());  
             var client = _httpClientFactory.CreateClient();
             var jsonData = JsonConvert.SerializeObject(model);
             StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
             var responseMessage = await client.PostAsync("http://localhost:5270/api/SendMessage", stringContent);
             if (responseMessage.IsSuccessStatusCode)
             {
-                return RedirectToAction("Index");
+                return RedirectToAction("Inbox");
             }
             return View();
         }
