f252d3691f100e6041e454d42cd68460be1d3bad
tuncmert <merttunc20@gmail.com>
2023-10-08 23:59:19 +0300

    update

--- Frontend/HotelProject.WebUI/Controllers/ContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/ContactController.cs
@@ -1,8 +1,12 @@
 ﻿using HotelProject.WebUI.Dtos.BookingDto;
 using HotelProject.WebUI.Dtos.ContactDto;
+using HotelProject.WebUI.Dtos.MessageCategoryDto;
+using HotelProject.WebUI.Dtos.RoomDto;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Net.Http;
 using System.Text;
 
@@ -17,8 +21,21 @@ namespace HotelProject.WebUI.Controllers
         {
             _httpClientFactory = httpClientFactory;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5270/api/MessageCategory");
+     
+             var jsonData = await responseMessage.Content.ReadAsStringAsync();
+             var values = JsonConvert.DeserializeObject<List<ResultMessageCategoryDto>>(jsonData);
+
+            List<SelectListItem> categoryValues = new List<SelectListItem>();
+            categoryValues = values.Select(x => new SelectListItem
+            {
+                Text = x.MessageCategoryName,
+                Value = x.MessageCategoryID.ToString()
+            }).ToList();
+            ViewBag.v = categoryValues;
             return View();
         }
         [HttpGet]
